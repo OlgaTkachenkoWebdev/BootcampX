f23bf8fc93ee454db0562f8b0852ce086fccd824
woodenonesie <olga.pi4ugina@yandex.ru>
2023-02-08 02:13:19 -0800

    add more queries

---
+++ 4_queries/5_average_cohort_assistance_time.sql
@@ -0,0 +1,6 @@
+SELECT AVG(completed_at - started_at) as average_assistance_time, cohorts.name as name
+FROM assistance_requests
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time;
